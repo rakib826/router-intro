2358f76c10b2498308489d7f5dfc82938e1d9078
Kazi Md. Rakibul Hassan <kazimdrakibulhassan@gmail.com>
2025-01-21 10:46:20 +0600

    user details card style added

--- src/components/UserDetails/UserDetails.jsx
+++ src/components/UserDetails/UserDetails.jsx
@@ -2,11 +2,18 @@ import { useLoaderData } from "react-router-dom";
 
 const UserDetails = () => {
   const user = useLoaderData();
-  const { name } = user;
+  const { name, address, phone } = user;
   return (
-    <div className="p-4 bg-gray-100 mt-2 mx-1 rounded-lg">
-      <h1>User Details Below</h1>
-      <h1>Name: {name} </h1>
+    <div className="p-4 bg-gray-100 mt-2 mx-1 rounded-lg text-center">
+      <h1 className="text-2xl font-bold">User Details</h1>
+      <div className=" p-4 w-fit mx-auto text-left mt-2  bg-green-400 rounded-md">
+        <h1 className="text-lg font-semibold">Name: {name} </h1>
+        <p>
+          Address : {address.street}, {address.suite}
+        </p>
+        <p>Zipcode: {address.zipcode} </p>
+        <p>Phone: {phone} </p>
+      </div>
     </div>
   );
 };
