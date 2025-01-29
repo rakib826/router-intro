1421c3bd968e3840c343eb0e73fe865c16dbafff
Kazi Md. Rakibul Hassan <kazimdrakibulhassan@gmail.com>
2025-01-29 21:03:39 +0600

    used navlink hook

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,14 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import './Header.css';
 const Header = () => {
     return (
         <div>
             <nav className="text-center p-4 flex flex-row items-center justify-end bg-green-900">
-                <Link to="/" className="py-2 px-4 font-semibold text-white bg-green-500 rounded-lg mx-2">Home</Link>
-                <Link to="/about" className="py-2 px-4 font-semibold text-white bg-green-500 rounded-lg mx-2">About</Link>
-                <Link to="/contact" className="py-2 px-4 font-semibold text-white bg-green-500 rounded-lg mx-2">Contact</Link>
-                <Link to="/users" className="py-2 px-4 font-semibold text-white bg-green-500 rounded-lg mx-2">Users</Link>
-                <Link to="/posts" className="py-2 px-4 font-semibold text-white bg-green-500 rounded-lg mx-2">Posts</Link>
+                <NavLink to="/" className="py-2 px-4 font-semibold text-white bg-green-500 rounded-lg mx-2">Home</NavLink>
+                <NavLink to="/about" className="py-2 px-4 font-semibold text-white bg-green-500 rounded-lg mx-2">About</NavLink>
+                <NavLink to="/contact" className="py-2 px-4 font-semibold text-white bg-green-500 rounded-lg mx-2">Contact</NavLink>
+                <NavLink to="/users" className="py-2 px-4 font-semibold text-white bg-green-500 rounded-lg mx-2">Users</NavLink>
+                <NavLink to="/posts" className="py-2 px-4 font-semibold text-white bg-green-500 rounded-lg mx-2">Posts</NavLink>
             </nav>
         </div>
     );
